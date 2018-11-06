4f22f98a4c5a516fc3d8c655145a3b9c301f9167
mudocker <mudocker@gmail.com>
2018-11-06 10:33:29 +0800

    1541469828

---
+++ sentinel/test.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+MASTER_IP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' redis-cluster_master_1)
+SLAVE_IP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' redis-cluster_slave_1)
+SENTINEL_IP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' redis-cluster_sentinel_1)
+docker exec redis-cluster_sentinel_1 redis-cli -p 26379 info Sentinel
+docker exec redis-cluster_sentinel_1 redis-cli -p 26379 SENTINEL get-master-addr-by-name mymaster
+
+docker pause redis-cluster_master_1
+sleep 10
+docker exec redis-cluster_sentinel_1 redis-cli -p 26379 info Sentinel
+docker exec redis-cluster_sentinel_1 redis-cli -p 26379 SENTINEL get-master-addr-by-name mymaster
+
+
+echo ------------------------------------------------
+echo Restart Redis master
+docker unpause redis-cluster_master_1
+sleep 5
+echo Current infomation of sentinel
+docker exec redis-cluster_sentinel_1 redis-cli -p 26379 info Sentinel
+echo Current master is
+docker exec redis-cluster_sentinel_1 redis-cli -p 26379 SENTINEL get-master-addr-by-name mymaster
